54a0d161bb0e3d7399ba7dc912ee126bb24979fd
Nisa0054 <113977816+Nisa0054@users.noreply.github.com>
2023-11-10 13:15:59 +0700

    /updateform

--- src/components/TestComponent2.vue
+++ src/components/TestComponent2.vue
@@ -1,7 +1,69 @@
 <template>
     <div>
-      <h2>{{ greeting }}</h2>
-      <form @submit.prevent="convertToGrams">
+      <h2>คำนวณแปลงหน่วยเบเกอรี่</h2>
+
+      <form action="">
+        <div class="container text-center">
+          <!-- หัวข้อ -->
+          <div class="row">
+            <div class="col">
+              <h5>ส่วนผสม</h5>
+            </div>
+            <div class="col">
+              <h5>ปริมาณ</h5>
+            </div>
+            <div class="col">
+              <h5>หน่วยเดิม</h5>
+            </div>
+            <div class="col">
+              <h5>หน่วยที่ต้องการแปลง</h5>
+            </div>
+            <div class="col">
+              <h5>แสดงผล</h5>
+            </div>
+          </div>
+
+          <!-- ช่อง -->
+          <div class="row">
+            <div class="col">
+              <input class="form-control" id="" type="text" placeholder="กรอกส่วนผสม" aria-label="default input example">
+            </div>
+            <div class="col">
+              <input class="form-control" id="" type="number" placeholder="ปริมาณส่วนผสม" aria-label="default input example">
+            </div>
+            <div class="col">
+              <select name="หน่วยเดิม" id="">
+                <option selected>เลือกหน่วย</option>
+                <option value="1">ถ้วยตวง(Cup)</option>
+                <option value="2">ช้อนโต๊ะ(Tablespoon)</option>
+                <option value="3">ช้อนชา(Teaspoon)</option>
+                <option value="4">ออนซ์(oz)</option>
+                <option value="5">มิลลิลิตร(ml)</option>
+                <option value="6">กรัม(g)</option>
+              </select>
+            </div>
+            <div class="col">
+              <select name="หน่วยที่ต้องการแปลง" id="">
+                <option selected>เลือกหน่วย</option>
+                <option value="1">ถ้วยตวง(Cup)</option>
+                <option value="2">ช้อนโต๊ะ(Tablespoon)</option>
+                <option value="3">ช้อนชา(Teaspoon)</option>
+                <option value="4">ออนซ์(oz)</option>
+                <option value="5">มิลลิลิตร(ml)</option>
+                <option value="6">กรัม(g)</option>
+              </select>
+            </div>
+            <div class="col">
+              <span id=""></span>
+            </div>
+
+          </div>
+
+
+         </div>
+       </form>
+      <!-- <h2>{{ greeting }}</h2> -->
+      <!-- <form @submit.prevent="convertToGrams">
         <label for="tspInput">Teaspoon:</label>
         <input type="number" id="tspInput" v-model="TSpoonTo_g" />
         <br />
@@ -19,7 +81,7 @@
         <br />
   
         <button type="submit">Convert to Grams</button>
-      </form>
+      </form> -->
     </div>
   </template>
   
@@ -28,7 +90,7 @@
     name: "TestComponent2",
     data() {
       return {
-        greeting: 'Hello, Vue!',
+        // greeting: 'Hello, Vue!',
         TSpoonTo_g: 5,
         TableSpoonTo_g: 24,
         CupTo_g: 240,

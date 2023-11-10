f4f818b4814609c5afab2d390ef5a7e6b62a123e
tawarukk <tawarukk@gmail.com>
2023-11-10 12:09:33 +0700

    tset

--- src/components/TestComponent2.vue
+++ src/components/TestComponent2.vue
@@ -1,7 +1,25 @@
 <template>
     <div>
       <h2>{{ greeting }}</h2>
-      <button @click="changeGreeting">Change Greeting</button>
+      <form @submit.prevent="convertToGrams">
+        <label for="tspInput">Teaspoon:</label>
+        <input type="number" id="tspInput" v-model="TSpoonTo_g" />
+        <br />
+  
+        <label for="tbspInput">Tablespoon:</label>
+        <input type="number" id="tbspInput" v-model="TableSpoonTo_g" />
+        <br />
+  
+        <label for="cupInput">Cup:</label>
+        <input type="number" id="cupInput" v-model="CupTo_g" />
+        <br />
+  
+        <label for="ozInput">Ounce:</label>
+        <input type="number" id="ozInput" v-model="OZTo_g" />
+        <br />
+  
+        <button type="submit">Convert to Grams</button>
+      </form>
     </div>
   </template>
   
@@ -11,22 +29,36 @@
     data() {
       return {
         greeting: 'Hello, Vue!',
+        TSpoonTo_g: 5,
+        TableSpoonTo_g: 24,
+        CupTo_g: 240,
+        OZTo_g: 28,
       };
     },
     methods: {
-      // เมทอดที่ใช้เปลี่ยนค่า greeting
       changeGreeting() {
         this.greeting = 'Hola, Vue!';
       },
-      // เมทอดที่รับค่าและแสดงใน console
       logMessage(message) {
         console.log(message);
+      },
+      convertToGrams() {
+        // ทำการคำนวณและแสดงผลลัพธ์ใน console
+        const tspInGrams = this.TSpoonTo_g * 5; // 1 ช้อนชา = 5 กรัม
+        const tbspInGrams = this.TableSpoonTo_g * 15; // 1 ช้อนโต๊ะ = 15 กรัม
+        const cupInGrams = this.CupTo_g * 240; // 1 ถ้วย = 240 กรัม
+        const ozInGrams = this.OZTo_g * 28.35; // 1 ออนซ์ = 28.35 กรัม
+  
+        console.log(`Teaspoon in grams: ${tspInGrams}`);
+        console.log(`Tablespoon in grams: ${tbspInGrams}`);
+        console.log(`Cup in grams: ${cupInGrams}`);
+        console.log(`Ounce in grams: ${ozInGrams}`);
       },
     },
   };
   </script>
   
-  <style>
+
+<style>
   /* สไตล์ตามต้องการ */
-  </style>
-  
+</style>

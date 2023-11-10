b9d86d706dbe5ea4358e9c92a985272007c52f2d
tawarukk <tawarukk@gmail.com>
2023-11-10 11:06:35 +0700

    edit file 01

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+ import TestComponent from "./components/TestComponent.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <TestComponent />
+  </div> 
 </template>
 
 <style scoped>
